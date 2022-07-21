381dafb1a4e1eb0f53d6a900bce6c4cd806dde77
Pedro Daniel Gonzalez Meza <viento100@gmail.com>
2022-07-20 22:51:06 -0400

    restructuracion de Controlador, y manejo de View.

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,11 +4,32 @@ namespace App\Controller;
 
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig',
+        [
+            'title' => 'Pb & Jams',
+            'tracks' => $tracks,
+        ]);
+    }
+
     #[Route('/browse/{slug}')]
     public function browser(string $slug = null) : Response
     {
